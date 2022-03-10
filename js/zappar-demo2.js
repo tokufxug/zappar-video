f9ce8e0d5bd6e526f2aa62581a8f395e8cb1c66a
Sadao Tokuyama <tokuyama@1planet.co.jp>
2022-03-10 17:37:30 +0900

    commit

--- js/zappar-demo2.js
+++ js/zappar-demo2.js
@@ -21,24 +21,24 @@ function initZappar()
         video.pause();
     });
 
-    document.getElementById('btn-scale-width-expansion').addEventListener('mouseup', function (e) {
+    document.getElementById('btn-scale-width-expansion').addEventListener('mousedown', function (e) {
         const s = movie.object3D.scale;
         movie.object3D.scale.set(s.x + 0.1, s.y, 1.0);
         document.getElementById("video-scale").innerText = "w:" + s.x + ",h:" + s.y;
     });
     
-    document.getElementById('btn-scale-width-shrink').addEventListener('mouseup', function (e) {
+    document.getElementById('btn-scale-width-shrink').addEventListener('mousedown', function (e) {
         const s = movie.object3D.scale;
         movie.object3D.scale.set(s.x - 0.1, s.y, 1.0);
         document.getElementById("video-scale").innerText = "w:" + s.x + ",h:" + s.y;
     });
-    document.getElementById('btn-scale-height-expansion').addEventListener('mouseup', function (e) {
+    document.getElementById('btn-scale-height-expansion').addEventListener('mousedown', function (e) {
         const s = movie.object3D.scale;
         movie.object3D.scale.set(s.x, s.y + 0.1, 1.0);
         document.getElementById("video-scale").innerText = "w:" + s.x + ",h:" + s.y;
     });
     
-    document.getElementById('btn-scale-height-shrink').addEventListener('mouseup', function (e) {
+    document.getElementById('btn-scale-height-shrink').addEventListener('mousedown', function (e) {
         const s = movie.object3D.scale;
         movie.object3D.scale.set(s.x, s.y - 0.1, 1.0);
         document.getElementById("video-scale").innerText = "w:" + s.x + ",h:" + s.y;
