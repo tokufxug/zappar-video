c54271a3ac9fb26c535f24430fce1cca687d7894
Sadao Tokuyama <tokuyama@1planet.co.jp>
2022-03-10 15:34:39 +0900

    commit

--- js/zappar-demo.js
+++ js/zappar-demo.js
@@ -14,29 +14,11 @@ function initZappar()
 
     zapparMarker.addEventListener("zappar-visible", () => {
         movie.setAttribute("visible", true);
-        if (zapparMarker.object3D)
-        {
-            zapparMarker.object3D.matrixAutoUpdate = true;
-            if (zapparMarker.object3D.scale)
-            {
-                let s = zapparMarker.object3D.scale;
-                console.log("SCALE:" + s.x + "," + s.y + "," + s.z);
-            }
-            else
-            {
-                console.log("SCALE Not Found");
-            }
-        }
-        else
-        {
-            console.log("Object3D Not Found");
-        }
         video.play();
     });
     zapparMarker.addEventListener("zappar-notvisible", () => {
         movie.setAttribute("visible", false);
         video.pause();
-        zapparMarker.object3D.matrixAutoUpdate = false;
     });
 }
 window.onload = onLoadFunction;
