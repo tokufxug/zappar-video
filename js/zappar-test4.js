1139da0108903e83268c15d6fc2b3cee2c0102fa
Sadao Tokuyama <tokuyama@1planet.co.jp>
2022-03-14 12:29:14 +0900

    commit

---
+++ js/zappar-test4.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function onLoadFunction()
+{
+    initZappar();
+}
+
+function initZappar()
+{
+    const zapparMarker = document.getElementById("zappar-marker");
+    const video = document.querySelector("#video");
+    const movie = document.getElementById("movie");
+    movie.setAttribute("visible", false);
+
+    zapparMarker.addEventListener("zappar-visible", () => {
+        movie.setAttribute("visible", true);
+        video.play();
+    });
+    zapparMarker.addEventListener("zappar-notvisible", () => {
+        movie.setAttribute("visible", false);
+        video.pause();
+    });
+}
+
+function round(num) {
+    var m = Number((Math.abs(num) * 100).toPrecision(15));
+    return Math.round(m) / 100 * Math.sign(num);
+}
+window.onload = onLoadFunction;
